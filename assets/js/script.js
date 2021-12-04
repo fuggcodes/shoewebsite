ce12f1638c3bd10a7dd2a85875fe4dbff285da89
Nick D <Ndefected15@gmail.com>
2021-12-04 12:50:29 -0500

    Working on retriving data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+let nameContainer = document.getElementById("name");
+//var imageContainer = document.querySelector("#");
+//var priceContainer = document.querySelector("#");
+//var infoContainer = document.querySelector("#");
+
 var shoeCollection = [];
 
 fetch("https://run.mocky.io/v3/5055ace0-da03-411c-8690-dfa88734e25e")
@@ -6,14 +11,22 @@ fetch("https://run.mocky.io/v3/5055ace0-da03-411c-8690-dfa88734e25e")
   })
   .then(function (data) {
     var results = data.results;
-    // console.log('api results', results);
+    //console.log("api results", results);
 
     for (var i = 0; i < results.length; i++) {
       shoeCollection.push({
         name: results[i].name,
         image: results[i].image.thumbnail,
+        price: results[i].retailPrice,
+        info: results[i].story,
       });
     }
-
-    console.log(shoeCollection);
   });
+
+console.log(shoeCollection);
+
+shoeCollection.forEach((item) => {
+  let li = document.createElement("li");
+  li.innerText = item;
+  nameContainer.appendChild(li);
+});
