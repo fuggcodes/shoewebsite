1850653c53c125a9d56793efbf34d1f6feff52a6
Abdulmelik Ersoy <kellemen23@gmail.com>
2021-12-11 23:43:54 -0500

    local storage fixed

--- assets/js/item-page.js
+++ assets/js/item-page.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(".ti-heart").on("click", function(){
         const image=$(this).parent().parent().parent().siblings().find(".default-img").attr("src");
 
-        const name=$(this).parent().parent().parent().parent().siblings().children().find("h5").text();
+        const name=$(this).parent().parent().parent().parent().siblings().children("h3").text();
         
         const keyName=$(this).parent().parent().parent().parent().siblings().children().attr("id");
 
@@ -13,6 +13,7 @@ $(document).ready(function(){
             shoeName: name
         }
         console.log(shoeData)
+        console.log(keyName)
         localStorage.setItem(keyName,JSON.stringify(shoeData));
     }); 
 });
